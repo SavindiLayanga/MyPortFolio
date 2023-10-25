6fcb3d7f96401e8645ec8d53616b2535d8fcfabd
SavindiLayanga <savindilayanga@gmail.com>
2023-10-25 12:37:40 +0530

    new one

---
+++ index.js
@@ -0,0 +1,13 @@
+// const assignmentCards = document.querySelectorAll('.assignment-card');
+//
+// assignmentCards.forEach(card => {
+//     card.addEventListener('mouseenter', () => {
+//         card.querySelector('.font-side').style.transform = 'rotateY(180deg)';
+//         card.querySelector('.back-side').style.transform = 'rotateY(0deg)';
+//     });
+//
+//     card.addEventListener('mouseleave', () => {
+//         card.querySelector('.font-side').style.transform = 'rotateY(0deg)';
+//         card.querySelector('.back-side').style.transform = 'rotateY(180deg)';
+//     });
+// });
